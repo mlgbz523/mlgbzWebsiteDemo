5ce6af4d49be69a47ada80a4152384f2a98aae2c
WuTianLi <91045167+mlgbz523@users.noreply.github.com>
2024-05-15 16:52:40 +0800

    Commit your changes before deploying

--- demo/vue-project/vite.config.js
+++ demo/vue-project/vite.config.js
@@ -5,7 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/' ,
+  base: process.env.NODE_ENV === 'production'
+    ? '/mlgbzWebsiteDemo/'
+    : '/',
   plugins: [
     vue(),
   ],
