d71d2d99556f7d863609db52d80b72df21ae22c8
WuTianLi <91045167+mlgbz523@users.noreply.github.com>
2024-05-16 01:45:23 +0800

    Add

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/' ,
+   base: '/mlgbzWebsiteDemo/' ,
   plugins: [
     vue(),
   ],
@@ -13,7 +13,7 @@ export default defineConfig({
     alias: {
       // 使用绝对路径或相对路径来指定 vue 包的位置
       vue: 'vue/dist/vue.esm-bundler.js', 
-      '@': fileURLToPath(new URL('./src', import.meta.url)) 
+      '@': fileURLToPath(new URL('/mlgbzWebsiteDemo//src', import.meta.url)) 
     }
   }
 })
