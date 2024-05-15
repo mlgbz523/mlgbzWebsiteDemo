c76e640dc59506fb1cde6a680465a416f2399ae3
wang fujing <91045167+mlgbz523@users.noreply.github.com>
2024-05-16 01:42:11 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,9 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      // 使用绝对路径或相对路径来指定 vue 包的位置
+      vue: 'vue/dist/vue.esm-bundler.js', 
+      '@': fileURLToPath(new URL('./src', import.meta.url)) 
     }
   }
 })
